5cf7f40d297560bdc4115ae31aa66be1c653b3c0
nargiz <mikhail.merkulov.89@internet.ru>
2024-07-27 15:53:48 +0600

    Последние изменения

--- shop-api/config.js
+++ shop-api/config.js
@@ -1,11 +1,14 @@
 const path = require('path');
 const rootPath = __dirname;
+
 module.exports = {
     rootPath,
     uploadPath: path.join(rootPath, 'public/uploads'),
     mongo: {
-        db: 'mongodb://localhost/shop-project',
-        options: {useNewUrlParser: true},
+        db: 'mongodb+srv://mamzigran:oVWph6fNqGZndqua@cluster0.xnmipzn.mongodb.net/shop-project?retryWrites=true&w=majority',
+        options: {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        },
     },
-
 };

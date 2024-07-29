05eb387b6c1a0c16154aa13731dcccfbc1332b15
nargiz <mikhail.merkulov.89@internet.ru>
2024-07-29 15:03:47 +0600

    connect cors

--- shop-api/index.js
+++ shop-api/index.js
@@ -14,7 +14,11 @@ const port = 8000;
 
 app.use(express.static('public'));
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: 'https://shop-frontend-michail.vercel.app', // Укажите ваш фронтенд URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use('/categories', categories);
 app.use('/users', users);
 app.use('/products', products);

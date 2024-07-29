f0d4b274d00e52b107644a095bd9edd071b2d964
nargiz <mikhail.merkulov.89@internet.ru>
2024-07-29 17:59:29 +0600

    Исправление багов на фронте

--- shop-frontend/src/components/ProductItem/ProductItem.js
+++ shop-frontend/src/components/ProductItem/ProductItem.js
@@ -4,17 +4,26 @@ import {Link} from "react-router-dom";
 import {Card, CardActions, CardContent, CardHeader, CardMedia, Grid, IconButton} from "@mui/material";
 import {ArrowForward} from "@mui/icons-material";
 import imageNotAvailable from '../../assets/image-not-available.jpg';
+import cpu from '../../assets/cpu.jpg';
+import hdd from '../../assets/hdd.jpg';
+import intel from '../../assets/intel.jpeg';
+import ncn from '../../assets/ncn.jpeg';
 import {apiUrl} from "../../config";
 const ProductItem = ({id, title, price, image}) => {
     let cardImage = imageNotAvailable;
     if (image) {
-        if (!image.includes('images')) {
-            cardImage = apiUrl+ '/images/' + image;
+        if (image.includes('intel')) {
+            cardImage = intel;
+        } else if (image.includes('cpu')) {
+            cardImage = cpu;
+        } else if (image.includes('hdd')) {
+            cardImage = hdd;
+        }else if (image.includes('ncn')) {
+            cardImage = ncn;
         } else {
             cardImage = apiUrl + '/' + image;
         }
     }
-    console.log(cardImage)
     return (
         <Grid item xs={12} sm={6} lg={3}>
             <Card sx={{height: '100%'}}>
